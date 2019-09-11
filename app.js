9be925444ef57bea937f363fa71fb5ffdf89ffe1
Keith Naber <knaber4124@gmail.com>
2019-09-10 22:51:23 -0400

    basic inquirer questions established

--- app.js
+++ app.js
@@ -34,30 +34,64 @@ inquirer.prompt([{
             }
         })
     }
-    else if(userTypeResponse.userType==='Manager'){
+    else if (userTypeResponse.userType === 'Manager') {
         inquirer.prompt([{
-            type:'password',
-            name:'manPassword',
-            message:'Enter The Manager Password'
-        }]).then(function(managerEntry){
-            if(managerEntry.manPassword===process.env.managerPassword){
-                console.log('You Entered the Correct Password');
+            type: 'password',
+            name: 'manPassword',
+            message: 'Enter The Manager Password'
+        }]).then(function (managerEntry) {
+            if (managerEntry.manPassword === process.env.managerPassword) {
+                console.log('Welcome To The Manager Portal');
+                inquirer.prompt([{
+                    type:'list',
+                    message:'What Action Do You Want To Perform?',
+                    choices:['View Products For Sale','View Low Inventory Items','Add Inventory','Add New Product'],
+                    name:'managerMenu'
+                }]).then(function(managerOption){
+                    let manFirstOption=managerOption.managerMenu;
+                    if(manFirstOption==='View Products For Sale'){
+                        console.log('Products for Sale');
+                    }
+                    else if(manFirstOption==='View Low Inventory Items'){
+                        console.log('Low Inventory');
+                    }
+                    else if(manFirstOption==='Add Inventory'){
+                        console.log('Add Inventory');
+                    }
+                    else if(manFirstOption==='Add New Product'){
+                        console.log('Add New Products');
+                    }
+                })
+
             }
-            else{
+            else {
                 console.log('WRONG PASSWORD!');
             }
         })
     }
-    else{
+    else {
         inquirer.prompt([{
-            type:'password',
-            name:'supPassword',
-            message:'Enter The Supervisor Password'
-        }]).then(function(supEntry){
-            if(supEntry.supPassword===process.env.supervisorPassword){
-                console.log('You Entered The Right Password');
+            type: 'password',
+            name: 'supPassword',
+            message: 'Enter The Supervisor Password'
+        }]).then(function (supEntry) {
+            if (supEntry.supPassword === process.env.supervisorPassword) {
+                console.log('Welcome To The Supervisor Portal');
+                inquirer.prompt([{
+                    type:'list',
+                    choices:['View Products By Department','Create New Department'],
+                    message:'What Action Would You Like To Perform?',
+                    name:'supMenu'
+                }]).then(function(supMenuResponse){
+                    if(supMenuResponse.supMenu==='View Products By Department'){
+                        console.log('Displaying Department Information');
+                    }
+                    else{
+                        console.log('Creating New Department');
+                    }
+                })
             }
-            else{
+            else {
                 console.log('WRONG PASSWORD!');
             }
         })
