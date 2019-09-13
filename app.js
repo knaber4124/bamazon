8f3dba5f08cada86686f0f1634fcbd2222d8596e
Keith Naber <knaber4124@gmail.com>
2019-09-13 19:29:36 -0400

    connected to mysql

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 let inquirer = require('inquirer');
 require('dotenv').config();
+let mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "naber2010",
+    database: "bamazon_db"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+});
 
 let env = process.env;
 
